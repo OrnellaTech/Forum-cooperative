fb0eeede6b708279f8dbb77f2510e52bf4f9ea7c
OrnellaTech <ornellakoffi05@gmail.com>
2024-11-10 16:37:58 +0000

    first commit

---
+++ file.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    var sticky = navbar.offsetTop;
+
+    if (window.scrollY >= sticky) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
+
+
